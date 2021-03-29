ae2eef7eebe7ed43b7e196c6a426fc327822e366
fandyekananda <61262017+fandyekananda@users.noreply.github.com>
2021-03-30 05:03:20 +0700

    Update adstera.js

--- adstera.js
+++ adstera.js
@@ -11,5 +11,5 @@
 	    $(this).removeClass('xepo_ads');
 
 	    //ubah google dengan ads direct link
-	    window.open('https://www.revenuecpmnetwork.com/hv30ks5d?key=ae8e664008b5e1835807bc3296aca24a', '_blank');
+	    window.open('https://www.highperformancecpmnetwork.com/hv30ks5d?key=ae8e664008b5e1835807bc3296aca24a', '_blank');
 	});
