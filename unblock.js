2dbd65799921d64fbcdf9aea56cbbaab3509c791
fandyekananda <61262017+fandyekananda@users.noreply.github.com>
2022-01-17 10:56:07 +0700

    Update unblock.js

--- unblock.js
+++ unblock.js
@@ -1,12 +1,8 @@
-$(document).ready(function()
-{
-$('body').addClass('xepo_ads');
-});
-
-$(document).on('click','.xepo_ads',function(e)
-{
-$(this).removeClass('xepo_ads');
-
-//ubah google dengan ads direct link
-window.open('https://www.highperformancecpmnetwork.com/hv30ks5d?key=ae8e664008b5e1835807bc3296aca24a', '_blank');
-});
+	$(document).ready(function() {
+		$('body').addClass('xepo_ads');
+	});
+	$(document).on('click', '.xepo_ads', function(e) {
+		$(this).removeClass('xepo_ads');
+		//ubah google dengan ads direct link
+		window.open('https://www.highperformancecpmnetwork.com/hv30ks5d?key=ae8e664008b5e1835807bc3296aca24a', '_blank');
+	});
